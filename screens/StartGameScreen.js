af587682dcb0e3621694e7b76b0a99f974d6b2e5
atharvabe19e <atharvabelge@gmail.com>
2023-07-12 12:05:08 +0530

    first

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const StartGameScreen = () => {
+  return (
+    <View>
+      <Text>StartGameScreen</Text>
+    </View>
+  )
+}
+
+export default StartGameScreens
+
+const styles = StyleSheet.create({})
